e47fa63f958cd72e0563ccc3079dfdb7bc63550b
00121kuri <00121kuri@gmail.com>
2024-01-22 19:14:23 +0900

    Add: ステップの経過によるスコアの遷移を表すグラフ作成機能を追加

---
+++ pages/plot-steps.py
@@ -0,0 +1,121 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+import time
+import os
+
+def plot_steps(uploaded_files, max_steps, file_name):
+    proggres_bar = st.progress(0, 'Loading Files...')
+    percent_complete = 0.0
+    # 経過時間の計測
+    start_time = time.time()
+
+    df_list = []
+
+    for uploaded_file in uploaded_files:
+        percent_complete += 1 / len(uploaded_files)
+        if percent_complete < 1.0:
+            proggres_bar.progress(percent_complete)
+        df = pd.read_csv(uploaded_file)
+        filled_df = fill_steps(df, max_steps)
+        df_list.append(filled_df)
+    
+    proggres_bar.empty()
+
+    # データフレームのリストを平均化
+    mean_df = df_list[0]
+    for i in range(1, len(df_list)):
+        mean_df += df_list[i]
+    mean_df /= len(df_list)
+    # 平均化したデータフレームを描画
+    y_axis_list = ['score', 'distanceScore', 'rotationScore', 'initChromosomeDiffScore', 'inputChromosomeDiffScore']
+    colors = ['blue', 'orange', 'green', 'red', 'purple']
+
+    export_folder = f'export/plot-steps/{file_name}'
+    
+    
+    for y_axis in y_axis_list:
+        fig = plt.figure(figsize=(8, 6))
+        plt.plot(mean_df['frameCount'], mean_df[y_axis], color=colors[y_axis_list.index(y_axis)])
+        plt.xlabel('step count')
+        plt.ylabel(y_axis)
+        plt.title(f'{y_axis}')
+        plt.grid(True)
+        # st.pyplot(fig)
+        if (file_name != ''):
+            # フォルダがなければ作成
+            if not os.path.exists(export_folder):
+                os.makedirs(export_folder)
+            fig.savefig(f'{export_folder}/{file_name}-{max_steps}-{y_axis}.png')
+            # pdf も出力
+            fig.savefig(f'{export_folder}/{file_name}-{max_steps}-{y_axis}.pdf')
+    
+    # グラフを並べて表示
+    fig = plt.figure(figsize=(10, 8))
+    for y_axis in y_axis_list:
+        if y_axis == 'score':
+            ax = plt.subplot(3, 1, 1)
+        else:
+            ax = plt.subplot(3, 2, y_axis_list.index(y_axis)+2)
+        plt.plot(mean_df['frameCount'], mean_df[y_axis], color=colors[y_axis_list.index(y_axis)])
+        plt.xlabel('step count')
+        plt.ylabel(y_axis)
+        plt.title(f'{y_axis}')
+        plt.grid(True)
+    plt.tight_layout()
+    st.pyplot(fig)
+
+    if (file_name != ''):
+        fig.savefig(f'{export_folder}/{file_name}-{max_steps}.png')
+        # pdf も出力
+        fig.savefig(f'{export_folder}/{file_name}-{max_steps}.pdf')
+    
+
+    # 経過時間の表示
+    elapsed_time = time.time() - start_time
+    st.write(f'Elapsed Time: {elapsed_time} [sec]')
+
+        
+
+def fill_steps(df, max_steps):
+    # proggres_bar = st.progress(0, 'Filling Steps...')
+    # percent_complete = 0.0
+
+    input_dict_array = df.to_dict('records')
+
+    filled_dict_array = []
+    filled_df = df.copy()
+    #fremeCountの値ががない場合は追加して前のフレームの値をコピー
+    step_index = -1
+    for i in range(-1, max_steps):
+        # percent_complete += 1 / max_steps
+        # if percent_complete < 1.0:
+        #     proggres_bar.progress(percent_complete)
+        if i not in df['frameCount'].values:
+            # # frameCountの値を更新
+            insert_dict = input_dict_array[step_index].copy()
+            insert_dict['frameCount'] = i
+            filled_dict_array.append(insert_dict)
+
+        else:
+            step_index += 1
+            filled_dict_array.append(input_dict_array[step_index])
+
+    filled_df = pd.DataFrame(filled_dict_array)
+    # proggres_bar.empty()
+
+            
+
+    # frameCountでソート
+    filled_df = filled_df.sort_values(['frameCount'], ascending=True).reset_index(drop=True)
+    return filled_df
+    
+
+
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+max_steps = st.number_input("Max Steps", 100, 100000, 50000)
+
+file_name = st.text_input("File Name", "")
+
+if st.button("Plot") and uploaded_files:
+    plot_steps(uploaded_files, max_steps, file_name)
