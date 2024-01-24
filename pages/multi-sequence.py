fb62f2ad66a1575a1744cd694a18128b8bde7d3c
00121kuri <00121kuri@gmail.com>
2024-01-24 15:32:21 +0900

    Add: 複数のシーケンスで各スコアのグラフを出力できるよう変更

--- pages/multi-sequence.py
+++ pages/multi-sequence.py
@@ -10,9 +10,10 @@ import plotly.io as pio
 import sys
 sys.path.append('..')
 from lib import db
+from lib.score import ScoreType
 
 
-def multi_sequence_viewer(sequenceIds, y_axis='score', col_num=1, folder_name=''):
+def multi_sequence_viewer(sequenceIds, col_num=1, folder_name=''):
     database = db.get_db()
     documents = database['result'].find({'sequenceId': {'$in': sequenceIds}})
     
@@ -41,22 +42,36 @@ def multi_sequence_viewer(sequenceIds, y_axis='score', col_num=1, folder_name=''
 
 
     # 複数のグラフを並べて表示, ユニークなsequenceIdの数に応じて行数を変更
-    size = 2
-    fig = plt.figure(figsize=(4 * size * int(col_num), 3 * size * math.ceil(len(sequenceIds)/col_num)))
-    # y軸の範囲を揃えるために最大値を取得
-    max_y = sequenceDf[y_axis].max()
-    for i, sequenceId in enumerate(sequenceDf_group['sequenceId']):
-        sequence = sequenceDf[sequenceDf['sequenceId'] == sequenceId]
-        # 順番に配置する
-        ax = fig.add_subplot(math.ceil(len(sequenceIds)/col_num), col_num, i+1)
-        ax.bar(sequence['frameCount'], sequence[y_axis])
-        ax.set_xlabel('Frame Count')
-        ax.set_ylim(0, max_y*1.1)
-        ax.set_ylabel(y_axis)
-        ax.set_title(f'{y_axis} Over Frame Count - Sequence ID: {sequenceId}')
-        ax.grid(True)
-    plt.tight_layout()
-    st.pyplot(fig)
+    for score_type in ScoreType:
+        # データに含まれていないscore_typeはスキップ
+        if score_type.name_db not in sequenceDf.columns:
+            continue
+        size = 2
+        fig = plt.figure(figsize=(4 * size * int(col_num), 3 * size * math.ceil(len(sequenceIds)/col_num)))
+        # y軸の範囲を揃えるために最大値を取得
+        max_y = sequenceDf[score_type.name_db].max()
+        for i, sequenceId in enumerate(sequenceDf_group['sequenceId']):
+            sequence = sequenceDf[sequenceDf['sequenceId'] == sequenceId]
+            # 順番に配置する
+            ax = fig.add_subplot(math.ceil(len(sequenceIds)/col_num), col_num, i+1)
+            ax.bar(sequence['frameCount'], sequence[score_type.name_db], color=score_type.color)
+            ax.set_xlabel('Frame Count')
+            ax.set_ylim(0, max_y*1.1)
+            ax.set_ylabel(score_type.label)
+            ax.set_title(f'{score_type.label} - Sequence ID: {sequenceId}')
+            ax.grid(True)
+        plt.tight_layout()
+        st.pyplot(fig)
+        # 画像を保存
+        if folder_name != '':
+            # フォルダがなければ作成
+            export_dir = f'export/multi-sequence/{folder_name}'
+            if not os.path.exists(export_dir):
+                os.makedirs(export_dir)
+            # pngを保存
+            plt.savefig(f'{export_dir}/{folder_name}-{score_type.name_db}.png')
+            # plt.savefig(f'export/multi-sequence/{folder_name}/{folder_name}-{score_type.name_db}.eps')
+            plt.savefig(f'{export_dir}/{folder_name}-{score_type.name_db}.pdf')
 
     # 表を作成
     sequenceDf_group_table =sequenceDf_group.drop(['optiSettingHash', 'envSettingHash', 'dateTime'], axis=1)
@@ -85,20 +100,13 @@ def multi_sequence_viewer(sequenceIds, y_axis='score', col_num=1, folder_name=''
 
     # 保存
     if folder_name != '':
-        # フォルダがなければ作成
-        if not os.path.exists(f'export/multi-sequence/{folder_name}'):
-            os.makedirs(f'export/multi-sequence/{folder_name}')
-        # pngを保存
-        plt.savefig(f'export/multi-sequence/{folder_name}/{folder_name}-{y_axis}.png')
-        plt.savefig(f'export/multi-sequence/{folder_name}/{folder_name}-{y_axis}.eps')
-        plt.savefig(f'export/multi-sequence/{folder_name}/{folder_name}-{y_axis}.pdf')
         # csvを保存
-        sequenceDf_group.to_csv(f'export/multi-sequence/{folder_name}/{folder_name}.csv', index=False)
+        sequenceDf_group.to_csv(f'{export_dir}/{folder_name}.csv', index=False)
         # texを保存
-        with open(f'export/multi-sequence/{folder_name}/{folder_name}.tex', mode='w') as f:
+        with open(f'{export_dir}/{folder_name}.tex', mode='w') as f:
             f.write(tex)
         # クエリを保存
-        with open(f'export/multi-sequence/{folder_name}/{folder_name}-query.txt', mode='w') as f:
+        with open(f'{export_dir}/{folder_name}-query.txt', mode='w') as f:
             f.write(str(sequenceIds))
     
     
@@ -111,7 +119,7 @@ st.title("Multi Sequence")
 
 
 sequenceIds = st.multiselect("Sequence ID", db.get_all_sequenceIds())
-y_axis = st.selectbox("Y Axis", ['score', 'distance', 'angleDiff'])
+# y_axis = st.selectbox("Y Axis", ['score', 'distance', 'angleDiff'])
 # グラフを並べる横の数
 col_num = st.slider("Column Num", 1, 5, 1)
 # 生成したグラフを保存
@@ -121,9 +129,9 @@ database = db.get_db()
 
 if st.button("Show") and sequenceIds:
     if file_name != '':
-        multi_sequence_viewer(sequenceIds, y_axis, col_num, file_name)
+        multi_sequence_viewer(sequenceIds, col_num, file_name)
     else:
-        multi_sequence_viewer(sequenceIds, y_axis, col_num)
+        multi_sequence_viewer(sequenceIds, col_num)
 else:
     st.write("Please input Sequence ID !")
 
